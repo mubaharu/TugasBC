5104d4a5993cf202c2fde9da2553e947387c27a7
mubaharu <sayabalya72@gmail.com>
2025-10-12 21:47:22 +0700

    Sesi ke-4

---
+++ Sesi4_menu_muhammad_balya_khaidir.js
@@ -0,0 +1,28 @@
+import { kalkulator } from './rumus.js';
+import readline from 'readline';
+
+// Membuat interface untuk input dari terminal
+const inputUser = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+inputUser.question('Masukkan angka pertama: ', angka1 => {
+    inputUser.question('Masukkan angka kedua: ', angka2 => {
+        inputUser.question('Pilih Operator yang digunakan (+, -, /, atau *): ', operator => {
+
+            // tambahkan input untuk angka kedua
+            // tambahkan input untuk memasukkan operator(+, -, *, /)
+            console.log(
+                `Hasil: ${kalkulator(
+                    parseFloat(angka1),
+                    parseFloat(angka2),
+                    operator
+                )}`
+            );
+
+            inputUser.close();
+        })
+    });
+});
+
